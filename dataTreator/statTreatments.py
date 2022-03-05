ed36c5193801262a51850d2b69fccb51b94df525
Samah37 <hs_kansab@esi.dz>
2022-03-05 10:42:13 +0100

    Updated: labels bar plot module, Added: Occurence label on graph

--- dataTreator/statTreatments.py
+++ dataTreator/statTreatments.py
@@ -1,6 +1,9 @@
 import collections
 import csv
+
+import numpy as np
 from matplotlib import pyplot
+import pandas as pd
 rows = []
 label_set= set()
 labels_general_list = []
@@ -21,15 +24,40 @@ with open('../output_data.csv', newline='') as csvfile:
 occurences = collections.Counter(labels_general_list)
 print(len(label_set))
 print(len(labels_general_list))
-print(occurences["regression"])
 #pyplot.figure(figsize=(50, 10))
-pyplot.rc('xtick', labelsize=4)
-pyplot.bar([j for j in label_set], [occurences[i]for i in (label_set)])
-pyplot.xlabel('The labels', )
-pyplot.savefig('test.png', dpi=None, facecolor='w', edgecolor='w',
-        orientation='portrait', papertype=None, format=None,
-        transparent=False, bbox_inches=None, pad_inches=0.1,
-        frameon=None, metadata=None)
+pyplot.rc('xtick', labelsize=6)
+
+x = np.arange(len(occurences))  # the label locations
+width = 0.35  # the width of the bars
+
+fig, ax = pyplot.subplots()
+rects1 = ax.bar([j for j in label_set], [occurences[i]for i in (label_set)], color = '#3E7DCC')
+
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Occurences')
+ax.set_title('Occurences by Labels')
+ax.set_xticks(x)
+ax.set_xticklabels(list(label_set),  rotation=45, ha='right')
+ax.legend()
+
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 1),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
+autolabel(rects1)
+
+
+fig.tight_layout()
+pyplot.savefig('../dataPlot/OccurencesByLabels.png')
 pyplot.show()
 
 
